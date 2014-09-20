a856b543e00195e8b15cdb3085426075a60317e9
RePod <repod@users.noreply.github.com>
2014-09-20 17:53:46 -0400

    PSDLE - Plus icon!
    Grab it right off the page, unifying PSDLE's appearance even more.
    Also changed the namespace from muh_games to psdle, finally.

--- psdle.user.js
+++ psdle.user.js
@@ -4,7 +4,7 @@
 // @description	Improving everyone's favorite online download list, one loop at a time. This will be updated infrequently, mostly for stability.
 // @namespace	https://github.com/RePod/psdle
 // @homepage	https://repod.github.io/psdle/
-// @version		1.032
+// @version		1.033
 // @require		https://code.jquery.com/jquery-1.11.1.min.js
 // @include		https://store.sonyentertainmentnetwork.com/*
 // @updateURL	https://repod.github.io/psdle/psdle.user.js
@@ -40,7 +40,7 @@ SOFTWARE.
 //TO-DO: Try to find the game API through the download list first, cannot confirm if US/en would work in every store territory/language (probably not).
 
 var repod = {};
-repod.muh_games = {
+repod.psdle = {
 	gamelist: [],
 	gamelist_cur: [],
 	entitlement_cache: {},
@@ -290,7 +290,8 @@ repod.muh_games = {
 			}
 		});
 		that.config.last_search = search;
-		$("#table_stats").html(this.gamelist_cur.length+((this.config.check_entitlements)?" (PS+: "+plus+")":"")+" / "+this.gamelist.length);
+		$("#table_stats").html(this.gamelist_cur.length+((this.config.check_entitlements)?" (<div id='psdleplus' style='display:inline-block' /> "+plus+")":"")+" / "+this.gamelist.length);
+		if (this.config.check_entitlements) { $("#psdleplus").css($(".psPlusIcon").css(["background-image","height","width","background-repeat","background-position"])); }
 		return temp;
 	},
 	genSearchOptions: function() {
@@ -480,5 +481,5 @@ repod.muh_games = {
 	}
 };
 
-var a = setInterval(function(a){ if ($("li.cellDlItemGame").length) { clearInterval(repod.muh_games.config.timerID); repod.muh_games.init(); } },1000);
-repod.muh_games.config = {"timerID":a};
+var a = setInterval(function(a){ if ($("li.cellDlItemGame").length) { clearInterval(repod.psdle.config.timerID); repod.psdle.init(); } },1000);
+repod.psdle.config = {"timerID":a};
