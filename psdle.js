83336052203e6871d299c43456bf6b7638aa9387
RePod <repod@users.noreply.github.com>
2015-02-26 04:36:05 -0500

    PSDLE - Validation
    Because Javascript is strict. Javastrict.
    
    Original joke.

--- psdle.js
+++ psdle.js
@@ -589,9 +589,9 @@ repod.psdle = {
 			if (data.default_sku && data.default_sku.entitlements.length == 1) {
 				var sys, type = "unknown", r = /^(PS(?:1|2)).+Classic$/i;
 				if (data.metadata) {
-					if (!!data.metadata.secondary_classification.values[0].match(r)) { sys = data.metadata.secondary_classification.values[0].match(r).pop(); }
+					if (data.metadata.secondary_classification && !!data.metadata.secondary_classification.values[0].match(r)) { sys = data.metadata.secondary_classification.values[0].match(r).pop(); }
 					//else if (!!data.metadata.game_subtype.values[0].match(r)) { sys = data.metadata.game_subtype.values[0].match(r).pop(); }
-					else if (!!data.metadata.primary_classification.values[0].match(r)) { sys = data.metadata.secondary_classification.values[0].match(r).pop(); }
+					else if (data.metadata.primary_classification && !!data.metadata.primary_classification.values[0].match(r)) { sys = data.metadata.primary_classification.values[0].match(r).pop(); }
 					else if (!!data.metadata.playable_platform) {
 						sys = [];
 						$.each(data.metadata.playable_platform.values,function(i,val) { sys.push(val.replace(/[^\w\d ]/g,"")) });
@@ -602,7 +602,7 @@ repod.psdle = {
 				
 			if (data.top_category == "tumbler_index") {
 				//We must go deeper.
-				if (data.metadata.secondary_classification.values[0] == "ADD-ON") { type = "add_on"; }
+				if (data.metadata.secondary_classification && data.metadata.secondary_classification.values[0] == "ADD-ON") { type = "add_on"; }
 			} else {
 				type = (data.top_category) ? data.top_category : "unknown";
 			}
