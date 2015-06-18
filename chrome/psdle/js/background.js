4ebd9fe0140edf03ea5cbc03270b228bb13b51bd
RePod <repod@users.noreply.github.com>
2015-06-18 17:51:27 -0400

    Chrome - The Fall of a Hero
    Remove PSDLE-kun's tragic story.
    
    Saves a couple hundred KB though.

--- chrome/psdle/js/background.js
+++ chrome/psdle/js/background.js
@@ -1,6 +1,5 @@
-function checkForValidUrl(tabId, changeInfo, tab) {
+function checkForValidUrl(tabId,changeInfo,tab) {
 	if (tab.url.indexOf('https://store.playstation.com/') == 0) {
-		console.log("Maybe Chrome-senpai will notice PSDLE-kun.");
 		chrome.pageAction.show(tabId);
 	}
 };
@@ -8,18 +7,9 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
 chrome.pageAction.onClicked.addListener(function (tab) {
-	/* Do stuff */
-	console.log("C-Chrome-senpai... I'm PSDLE-kun.");
-
 	//chrome.pageAction.hide(tab.id);
-	console.log("Querying injection status.");
-	chrome.tabs.sendMessage(tab.id, {alive: "yes"}, function(response) {
-        if (response) {
-            console.log("Attempted to inject PSDLE on the current tab but it was previously injected!");
-        }
-        else {
-            console.log("Injecting PSDLE.");
-			chrome.tabs.executeScript({file: "js/chrome.js"});
-        }
+	chrome.tabs.sendMessage(tab.id, {alive: "y"}, function(response) {
+        if (response) { console.log(response); }
+        else { chrome.tabs.executeScript({file: "js/chrome.js"}); }
     });
 });
--- chrome/psdle/js/chrome.js
+++ chrome/psdle/js/chrome.js
@@ -1,16 +1,13 @@
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.alive == "yes") {
-            sendResponse({message: "Baby baby baby baby baby baby baby baby baby baby baby baby yeaaaaaaaaaaaaaaah."});
-			console.log("PSDLE's chrome.js received an alive request.\nThe fact it knows this means it's already been injected!\nPlease reload the page if you would like to inject it again.");
-			alert(chrome.i18n.getMessage("psdleRunning"));
-		}
- });
- 
-console.log("Surgical procedure. Scalpel.");
+    function(request,sender,sendResponse) {
+        if (request.alive == "y") {
+            sendResponse({message:"ELDSP"});
+            alert(chrome.i18n.getMessage("psdleRunning"));
+        }
+    }
+);
+
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('js/psdle.js');
-console.log("PSDLE-kun has been injected into Chrome-senpai.");
 s.onload = function() { this.parentNode.removeChild(this); };
 (document.head||document.documentElement).appendChild(s);
-console.log("chrome.js dies a heroic death. The End.");
