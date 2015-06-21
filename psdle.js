0491988aa3cb8faa128a98bf51e285ebc55c7a94
RePod <repod@users.noreply.github.com>
2015-06-21 02:19:23 -0400

    PSDLE - Minor isValidContent() change.
    TV, TV. TV TV TV TV. TV. TV? TV...

--- psdle.js
+++ psdle.js
@@ -316,9 +316,8 @@ repod.psdle = {
         var exp = (obj.license) ? obj.license.expiration : obj.inactive_date,
             inf = (obj.license) ? obj.license.infinite_duration : false;
         
-        if (new Date(exp) < new Date() && inf) { return 0; }
-        else if (obj.VUData) { return 0; }
-        else if (obj.drm_def && obj.drm_def.contentType == "TV") { return 1; }
+        if (obj.VUData || (obj.drm_def && obj.drm_def.contentType == "TV")) { return 0; }
+        else if (new Date(exp) < new Date() && inf) { return 0; }
         else if (obj.drm_def || obj.entitlement_attributes) { return 1; }
         else { return 0; }
     },
