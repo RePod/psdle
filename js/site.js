4f8eedbc154695f17541aa55974dea41609d5952
RePod <RePod@users.noreply.github.com>
2016-04-19 01:02:23 -0400

    GH-PAGES - Can't stop saving!
    1493 to 921 bytes! 0.55KB more, 0.95KB saved total!
    
    Must stop before I OCD with this.
    Semicolons are for squares, and I probably triggered somebody.

--- js/site.js
+++ js/site.js
@@ -1,21 +1,14 @@
-//Default javascript for PSDLE's website. Generally Google Analytics integration and fancy stuff.
-$(document).ready(function() {
-    //Select method
-    $("#method_select > label").click(function() {
-        $("#method_holder .collapse.in").not($(this).data("target")).collapse("hide");
-        $("#method_holder "+$(this).data("target")).collapse("show");
-        ga('send','event','Getting Started','click',$(this).text().trim());
-    });
-    if(window.chrome !== null && window.chrome !== undefined && window.navigator.vendor === "Google Inc."){$("label[data-target=#method_extension]").click();}
-    //Events
-    $("#paq_more").click(function(){ga('send','event','P.A.Q.','click','More...');});
-    //Navbar
-    $(".navbar-toggle").click(function(){ga('send','event','Navbar','click','Toggle');});
-    $("nav li").click(function(){ga('send','event','Navbar','click',$(this).text().trim());});
-    //Conversions
-    $("#inline_code").on("contextmenu",function(){ga('send','event','Method','contextmenu','Direct',1);});
-    $("#inline_userscript").on("click",function(){ga('send','event','Method','click','Userscript',1);});
-    $("#inline_userscript").on("contextmenu",function(){ga('send','event','Method','contextmenu','Userscript',1);});
-    $("#inline_bookmarklet").on("click",function(){ga('send','event','Method','click','Bookmarklet',1);});
-    $("#inline_bookmarklet").on("contextmenu",function(){ga('send','event','Method','contextmenu','Bookmarklet',1);});
-});
+$(document).ready(function(){
+ $("#method_select > label").click(function(){
+  var t=$(this).data("target")
+  $("#method_holder .collapse.in").not(t).collapse("hide")
+  $("#method_holder "+t).collapse("show")
+  ga('send','event','Getting Started','click',$(this).text().trim())
+ })
+ if(window.chrome&&window.navigator.vendor==="Google Inc."){$("label[data-target=#method_extension]").click()}
+ $("#paq_more").click(function(){ga('send','event','P.A.Q.','click','More...')})
+ $(".navbar-toggle").click(function(){ga('send','event','Navbar','click','Toggle')})
+ $("nav li").click(function(){ga('send','event','Navbar','click',$(this).text().trim())})
+ $("#inline_code").on("contextmenu",function(){ga('send','event','Method','contextmenu','Direct',1)})
+ $("#inline_bookmarklet,#inline_userscript").on("contextmenu click",function(e){ga('send','event','Method',e.type,$(this).attr("id").split("_").pop(),1)})
+})
