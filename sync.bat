16286c6c219b0fdcbc74ca444b33b72e0e088fcc
RePod <RePod@users.noreply.github.com>
2016-04-19 02:00:51 -0400

    Single-line sync.

--- sync.bat
+++ sync.bat
@@ -1,4 +1,2 @@
 ::Switch to gh-pages, pull from master, commit, then switch back to master.
-git checkout gh-pages
-git checkout master psdle*.js
-git checkout master
+git checkout gh-pages && git checkout master psdle*.js && git checkout master
