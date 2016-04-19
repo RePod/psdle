da92ced7caba4a03b501c79a106e0cca636b86a1
RePod <RePod@users.noreply.github.com>
2016-04-19 01:44:12 -0400

    Chrome - Update 7-Zip batch.
    Includes the min by default now and renames it accordingly.

--- chrome/7-Zip.bat
+++ chrome/7-Zip.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" u -uq0 -mx9 psdle.zip -mtc- ./psdle/* -r -x!*.db -x!*.ini
+"C:\Program Files\7-Zip\7z.exe" a -uq0 -mx9 psdle.zip -mtc- ./psdle/* ../psdle.min.js -r -x!*.db -x!*.ini
+"C:\Program Files\7-Zip\7z.exe" rn psdle.zip psdle.min.js js\psdle.js
