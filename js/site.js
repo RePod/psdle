5286c7cd5d0fd564f738b8b10195841fe4659625
RePod <RePod@users.noreply.github.com>
2016-05-01 10:47:44 -0400

    GH-PAGES - Improve suggested methods.
    Basically a huge if/not Chrome and if/not mobile.
    Mobile Chrome users don't need to be suggested the desktop extension,
    and they can't use the bookmarklet.
    
    Unfortunately by not suggesting the bookmarklet, mobile users don't get
    30kb in savings from using the min version.

--- js/site.js
+++ js/site.js
@@ -5,7 +5,12 @@ $(document).ready(function(){
   $("#method_holder "+t).collapse("show")
   ga('send','event','Getting Started','click',$(this).text().trim())
  })
- if(window.chrome&&window.navigator.vendor==="Google Inc."){$("label[data-target=#method_extension]").click()}
+ var chrome=(window.chrome&&window.navigator.vendor==="Google Inc."),
+     mobile=(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini|mobile/ig.test(navigator.userAgent));
+ if(chrome && !mobile){$("label[data-target=#method_extension]").click()}
+ else if(chrome && mobile){}//Direct
+ else if (!chrome && mobile){$("label[data-target=#method_bookmarklet]").click()}
+ else if (mobile){$("label[data-target=#method_direct]").click()}
  $("#paq_more").click(function(){ga('send','event','P.A.Q.','click','More...')})
  $(".navbar-toggle").click(function(){ga('send','event','Navbar','click','Toggle')})
  $("nav li").click(function(){ga('send','event','Navbar','click',$(this).text().trim())})
