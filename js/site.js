545a902fbcc744f1442043c67e93429f9023a677
RePod <RePod@users.noreply.github.com>
2016-05-02 06:48:32 -0400

    GH-PAGES - Perfect?
    Step two.
    
    Now swaps out the Direct's URL to .min if supposedly on mobile.
    Best of both worlds:
    - Using .min previously on Bookmarklet
    - Not suggesting Bookmarklet when it's flaky on mobile

--- js/site.js
+++ js/site.js
@@ -6,11 +6,9 @@ $(document).ready(function(){
   ga('send','event','Getting Started','click',$(this).text().trim())
  })
  var chrome=(window.chrome&&window.navigator.vendor==="Google Inc."),
-     mobile=(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini|mobile/ig.test(navigator.userAgent));
+     mobile=(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini|mobile/ig.test(navigator.userAgent))
  if(chrome && !mobile){$("label[data-target=#method_extension]").click()}
- else if(chrome && mobile){}//Direct
- else if (!chrome && mobile){$("label[data-target=#method_bookmarklet]").click()}
- else if (mobile){$("label[data-target=#method_direct]").click()}
+ if (mobile){var a=$("#psdle_bit");a.text(a.text().replace(".js",".min.js"));}//min on mobile.
  $("#paq_more").click(function(){ga('send','event','P.A.Q.','click','More...')})
  $(".navbar-toggle").click(function(){ga('send','event','Navbar','click','Toggle')})
  $("nav li").click(function(){ga('send','event','Navbar','click',$(this).text().trim())})
