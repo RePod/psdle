f80597aa6a6b33f8f32fe534f08b6b3f8e95e543
RePod <RePod@users.noreply.github.com>
2017-09-25 14:24:54 -0400

    Deploy-sync - Redundancy

--- deploy-sync.bat
+++ deploy-sync.bat
@@ -1,4 +1,6 @@
 ::I use PortableGit\bin in PATH
 git init
+::Ensure we have these for the next step
+git checkout master && git commit -a -m "Deployment" && git push
 ::Switch to gh-pages, pull from master, commit, then switch back to master.
 git checkout gh-pages && git checkout master psdle*.js && git commit -a -m "Sync with master." && git push && git checkout master
