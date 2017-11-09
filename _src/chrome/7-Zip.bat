90cc189fe183077cc5a2cb7493d36bd241dacba9
RePod <RePod@users.noreply.github.com>
2017-11-09 16:10:13 -0500

    Fix 7-zip

--- _src/chrome/7-Zip.bat
+++ _src/chrome/7-Zip.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a -uq0 -mx9 %~dp0/psdle.zip -mtc- %~dp0/psdle/* %~dp0../../_dist/psdle.min.js -r -x!*.db -x!*.ini
+"C:\Program Files\7-Zip\7z.exe" a -uq0 -mx9 %~dp0/psdle.zip -mtc- %~dp0/psdle/* %~dp0../../psdle.min.js -r -x!*.db -x!*.ini
 "C:\Program Files\7-Zip\7z.exe" rn %~dp0/psdle.zip psdle.min.js js\psdle.js
