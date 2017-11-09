876a254d3e409952528e3a7b1e5eadf2489e03b6
RePod <RePod@users.noreply.github.com>
2017-11-09 16:00:35 -0500

    Deployment

--- deploy-sync.bat
+++ deploy-sync.bat
@@ -3,4 +3,4 @@ git init
 ::Ensure we have these for the next step
 git checkout master && git commit -a -m "Deployment" && git push
 ::Switch to gh-pages, pull from master, commit, then switch back to master.
-git checkout gh-pages && git checkout master psdle*.js && git commit -a -m "Sync with master." && git push && git checkout master
+git checkout gh-pages && git checkout master _src/psdle*.js && git commit -a -m "Sync with master." && git push && git checkout master
