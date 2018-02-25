0dae32f0e839558c05f0b40e1abe7262f1034626
RePod <RePod@users.noreply.github.com>
2018-02-25 12:51:44 -0500

    Live and learn

--- _src/chrome/7-Zip.bat
+++ _src/chrome/7-Zip.bat
@@ -3,4 +3,4 @@
 ::"C:\Program Files\7-Zip\7z.exe" rn %~dp0/psdle.zip psdle.min.js js\psdle.js
 
 ::Assuming symlink: mklink /H "_src/chrome/psdle/js/psdle.js" "psdle.min.js"
-"C:\Program Files\7-Zip\7z.exe" a -uq0 -mx9 %~dp0/psdle.zip -mtc- %~dp0/psdle/* -r -x!*.db -x!*.ini
+"%PROGRAMFILES%\7-Zip\7z.exe" a -uq0 -mx9 %~dp0/psdle.zip -mtc- %~dp0/psdle/* -r -x!*.db -x!*.ini
