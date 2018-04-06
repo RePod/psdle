f7e9f2888ebf1b1b6c364a472826265b5122155c
RePod <RePod@users.noreply.github.com>
2018-04-05 20:26:56 -0400

    Chrome - Deploy also opens Firefox dev page

--- _src/chrome/deploy.bat
+++ _src/chrome/deploy.bat
@@ -1,2 +1,3 @@
 ::Open the chrome extension editing page on webstore.
 explorer "https://chrome.google.com/webstore/developer/edit/jdjhhapoddhnimgdemnpbfagndcnmhii"
+explorer "https://addons.mozilla.org/en-US/developers/addon/psdleforfirefox/versions/submit/"
