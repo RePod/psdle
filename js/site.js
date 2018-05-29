29512068ca6532eb6468ef540d771560cc7d43ee
RePod <RePod@users.noreply.github.com>
2018-05-29 10:39:19 -0400

    Improve noscript functionality
    Assume noscript in this post-GDPR world so a lot of the site isn't hidden, RIP mobile scrolling.
    Could alternatively do more stuff with CSS, too lazy to spawn elements via JS.
    
    Tightened FF57 detection.
    Better method labels when stacked for noscript.
    Another opt-out option.
    SVG logo link.

--- js/site.js
+++ js/site.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
+ $("#paq_container").addClass("collapse") //Auto-collapse when we can script
  $("#method_select > label").click(function(){
   var t=$(this).data("target")
-  $("#method_holder .collapse.in").not(t).collapse("hide")
+  $("#method_holder > *").not(t).collapse("hide")
   $("#method_holder "+t).collapse("show")
   //ded: ga('send','event','Getting Started','click',$(this).text().trim())
  });
  var ff57regex = /(?:rv:|Firefox\/)([\d\.]+)/i,
- ff57 = (ff57regex.test(navigator.userAgent) && parseInt(navigator.userAgent.match(ff57regex).pop()) >= 57)
- var chrome=(window.chrome&&window.navigator.vendor==="Google Inc."),
-     ff=(typeof InstallTrigger !== undefined)
+     ff57 = (ff57regex.test(navigator.userAgent) && parseInt(navigator.userAgent.match(ff57regex).pop()) >= 57),
+     chrome=(window.chrome&&window.navigator.vendor==="Google Inc."),
      mobile=(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini|mobile/ig.test(navigator.userAgent))
- if((chrome || (ff && ff57)) && !mobile){$("label[data-target=#method_extension]").click()}
+ if((chrome || ff57) && !mobile){$("label[data-target=#method_extension]").click()}
  if (mobile){var a=$("#psdle_bit");a.text(a.text().replace(".js",".min.js"));}//min on mobile.
  /*
  $("#paq_more").click(function(){ga('send','event','P.A.Q.','click','More...')})
