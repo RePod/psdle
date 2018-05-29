8bd129189a6f0c55bcb06b5b8e513d205ca910e7
RePod <RePod@users.noreply.github.com>
2018-05-29 09:33:06 -0400

    Disable some tracking events
    Unironically, I never looked at them nor trusted them to be accurate.

--- js/site.js
+++ js/site.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var t=$(this).data("target")
   $("#method_holder .collapse.in").not(t).collapse("hide")
   $("#method_holder "+t).collapse("show")
-  ga('send','event','Getting Started','click',$(this).text().trim())
+  //ded: ga('send','event','Getting Started','click',$(this).text().trim())
  });
  var ff57regex = /(?:rv:|Firefox\/)([\d\.]+)/i,
  ff57 = (ff57regex.test(navigator.userAgent) && parseInt(navigator.userAgent.match(ff57regex).pop()) >= 57)
@@ -12,9 +12,11 @@ $(document).ready(function(){
      mobile=(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Opera Mini|mobile/ig.test(navigator.userAgent))
  if((chrome || (ff && ff57)) && !mobile){$("label[data-target=#method_extension]").click()}
  if (mobile){var a=$("#psdle_bit");a.text(a.text().replace(".js",".min.js"));}//min on mobile.
+ /*
  $("#paq_more").click(function(){ga('send','event','P.A.Q.','click','More...')})
  $(".navbar-toggle").click(function(){ga('send','event','Navbar','click','Toggle')})
  $("nav li").click(function(){ga('send','event','Navbar','click',$(this).text().trim())})
  $("#inline_code").on("contextmenu",function(){ga('send','event','Method','contextmenu','Direct',1)})
  $("#inline_bookmarklet,#inline_userscript").on("contextmenu click",function(e){ga('send','event','Method',e.type,$(this).attr("id").split("_").pop(),1)})
+ */
 })
