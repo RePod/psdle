0b4f264596ed9f7a905a0434458a6f1e3e76b865
RePod <RePod@users.noreply.github.com>
2020-10-29 12:48:25 -0400

    PSDLE - The Node stuff
    Update modules
    Switch to uglify-es for arrow function support

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,7 +100,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-includes');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-string-replace');
