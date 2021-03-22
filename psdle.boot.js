e2ec40f72d6a086a951108e8c27983916b5ede46
RePod <RePod@users.noreply.github.com>
2021-03-22 13:25:50 -0400

    Back compat 101

---
+++ psdle.boot.js
@@ -0,0 +1,9 @@
+// Whoa so dynamic. Keep a backup since this won't last long enough to warrant changing the fickle "build system".
+(()=>{
+const legacy = location.href.indexOf("psapp") > 0 ? "3" : "4";
+const psdleURL = `//repod.github.io/psdle/psdle${legacy}.js`
+const script = document.createElement('script');
+script.async = true;
+script.src = psdleURL;
+document.body.appendChild(script);
+})()
