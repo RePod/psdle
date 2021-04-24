4bd07949b92e5c8aaaa21f8b2e270e9477c170da
RePod <RePod@users.noreply.github.com>
2021-04-24 12:59:06 -0400

    Update deploy-sync.bat

--- deploy-sync.bat
+++ deploy-sync.bat
@@ -1,16 +1,4 @@
 git init
 git checkout master && git commit -a -m "Deployment" && git push
 
-git checkout gh-pages
-git checkout master -- _src/base/*/psdle.*.*.js
-
-move _src\base\gotham\psdle.*.*.js
-move _src\base\valkyrie\psdle.*.*.js
-move psdle.gotham.includes.js psdle.gotham.js
-move psdle.valkyrie.includes.js psdle.valkyrie.js
-
-git restore --staged _src/
-git add psdle*.js
-
-git commit -a -m "Sync with master."
-git push
+git checkout gh-pages && sync.bat
