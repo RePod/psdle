b003df0ed6f5ebf96084a7f58f9c53ec6a58d0c6
RePod <RePod@users.noreply.github.com>
2024-07-08 18:14:21 -0400

    PSDLE - MV3 & Simplify
    Everything is Chrome in the future...
    Greatly simplify the extension. This probably could have been done with V2 but here we are.

--- _src/chrome/psdle/js/chrome.js
+++ _src/chrome/psdle/js/chrome.js
@@ -2,34 +2,11 @@ console.info("PSDLE Chrome | *radio static* I'm in",window,document)
 var isValkyrie = location.hostname == "transact.playstation.com"
 var variant = isValkyrie ? "valkyrie" : "gotham"
 
-chrome.runtime.onMessage.addListener(
-    function(request,sender,sendResponse) {
-        if (request.alive == "y") {
-            sendResponse({message:"ELDSP"});
-            alert(chrome.i18n.getMessage("psdleRunning"));
-        }
-    }
-);
-
-function spawn(url) {
-    console.info("PSDLE Chrome | Spawn",url);
-    var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.onload = function() {
-        this.parentNode.removeChild(this);
-    };
-    s.onerror = function(e) {
-        console.warn("PSDLE Chrome | Something broke.",url);
-    };
-    s.src = chrome.extension.getURL(url);
-    (document.head||document.documentElement).appendChild(s);
+if (!isValkyrie) {
+    var qualityElementDetector5000 = setInterval(function() {
+        clearInterval(qualityElementDetector5000)
+        document.querySelector(".psw-drawer").style['max-width'] = '95%'
+        document.querySelector(".psw-drawer").style['min-width'] = '95%'
+        document.querySelector(".psw-drawer").style['width'] = '100%'
+    }, 1000)
 }
-
-(()=>{
-    console.info("PSDLE Chrome | isValkyrie",isValkyrie);
-    spawn("js/psdleChromium.js");
-    spawn(`js/psdle.${variant}.js`);
-})()
-
-// Pass it up.
-isValkyrie;
